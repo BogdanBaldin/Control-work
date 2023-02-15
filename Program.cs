7363c38081099d7b1947b253da85299664db4e0d
Bogdan <hurasy71@gmail.com>
2023-02-16 09:29:46 +1000

    Test1

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.Write("Сколько элементов Вы хотите ввести? ");
-size int = Преобразовать.ToInt32(консоль.ReadLine());
+size int = Convert.ToInt32(Console.ReadLine());
 
 string[] arrayStrings = new string[size];
 for (int i = 0; i < size; i++)
 {
  Console.WriteLine($"Введите {i+1}-й элемент: ");
- string element = Convert.ToString(консоль.ReadLine());
+ string element = Convert.ToString(Console.ReadLine());
  arrayStrings[i] = element;
 }
 
